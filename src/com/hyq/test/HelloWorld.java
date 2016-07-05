65f1c514bf960d4a90265e4105f151b623e451f3
hyq <411125759@qq.com>
2016-07-05 10:05:32 +0800

    第一次更新操作

--- src/com/hyq/test/HelloWorld.java
+++ src/com/hyq/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hyq.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("µ⁄“ª¥Œ≤‚ ‘£°£°£°");
+		System.out.println("Hello,World!!!");
 	}
 
 }
